a8ae5f20c66f6fad342705f528413a4b01fb0b21
narasimha devadurgam <narasimha.devadurgam@entainindia.com>
2026-05-27 20:29:57 +0530

    remove temp batch file

--- fix.bat
+++
@@ -1,5 +0,0 @@
-cd /d C:\Users\Narasimha.Devadurgam\source\repos\smart-parking-lot-system
-del do.bat
-git add -A
-git commit -m "fix: proper error logging in withLock, remove temp files"
-git push origin main
